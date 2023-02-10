11ed8c8b520b2aa618a3e231fab9c5233a12545e
michaelsmorgon <smorgon@int.pl>
2023-02-10 22:44:39 +0100

    feat: add checking brackets string

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = str.split('').reduce((acc, bracket) => {
+        let stackLength = acc.length;
+        if (stackLength === 0) {
+            acc.push(bracket);
+        } else {
+            let findPair = bracketsConfig.find(value => {
+                if (value[1] === bracket) {
+                    return true;
+                }
+            });
+            if (findPair === undefined) {
+                acc.push(bracket);
+                return acc;
+            }
+            if (acc[stackLength - 1] === findPair[0]) {
+                acc.pop();
+            } else {
+                acc.push(bracket);
+            }
+        }
+        return acc;
+    }, []);
+    return !stack.length;
 }
